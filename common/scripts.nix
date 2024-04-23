03163d7a98f025a955d5c7f16671dfc2039dab56
Lukas Santner <lukas@santi.gg>
2024-04-23 14:42:38 +0100

    fix ts

--- common/scripts.nix
+++ common/scripts.nix
@@ -5,38 +5,38 @@ with pkgs; [
       name = "ts";
       runtimeInputs = with pkgs; [tmux fzf];
       text = ''
-        #!/usr/bin/env bash
+set +e
+set +o nounset
 
-        # SHAMELESSLY STOLEN FROM https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+# SHAMELESSLY STOLEN FROM https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
 
-        if [[ $# -eq 1 ]]; then
-            selected=$1
-        else
-            selected=$(find ~/Projects/co ~/Projects/p ~/Projects/w -mindepth 1 -maxdepth 1 -type d | fzf)
-        fi
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(find ~/Projects/co ~/Projects/p ~/Projects/w -mindepth 1 -maxdepth 1 -type d | fzf)
+fi
 
-        if [[ -z $selected ]]; then
-            exit 0
-        fi
+if [[ -z $selected ]]; then
+    exit 0
+fi
 
-        selected_name=$(basename "$selected" | tr . _)
-        tmux_running=$(pgrep tmux)
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
 
-        if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-            tmux new-session -s "$selected_name" -c "$selected"
-            exit 0
-        fi
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s "$selected_name" -c "$selected"
+    exit 0
+fi
 
-        if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-            tmux new-session -ds "$selected_name" -c "$selected"
-        fi
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
+fi
 
-        if [[ -z $TMUX ]]; then
-        		tmux attach -t "$selected_name"
-        fi
+if [[ -z $TMUX ]]; then
+		tmux attach -t "$selected_name"
+fi
 
-        tmux switch-client -t "$selected_name"
-
+tmux switch-client -t "$selected_name"
       '';
     }
   )
