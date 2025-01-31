de8e7f8833f0f14d880b802a2591fd9654b08880
Lukas Santner <lukas@santi.gg>
2025-01-31 17:22:30 +0000

    fix

--- linux/home.nix
+++ linux/home.nix
@@ -4,16 +4,6 @@
   inputs,
   ...
 }: {
-  home-manager.users.santi = {
-    config,
-    pkgs,
-    ...
-  }: {
-    imports = [../shared/home];
-    home.file = {
-      ".config/nvim" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/configs/nvim";
-      };
-    };
-  };
+  imports = [../shared/home];
+  home.file = {};
 }
