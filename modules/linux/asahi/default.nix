0a549a16621373d073071d21c6d2ab8935ff5dad
Lukas Santner <lukas@santi.gg>
2025-02-02 17:02:51 +0000

    asahi: remove gui stuff

--- modules/linux/asahi/default.nix
+++ modules/linux/asahi/default.nix
@@ -9,13 +9,4 @@
       asahi-bless
     ]
     ++ (import ./scripts.nix {inherit pkgs;});
-
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.xserver = {
-    enable = true;
-    xkb.layout = "de";
-    xkb.variant = "mac";
-    autorun = true;
-  };
 }
