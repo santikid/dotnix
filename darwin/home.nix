334a0d148d25d1eb1e3cfe9f6fcd3f478637d62a
Lukas Santner <lukas@santi.gg>
2025-02-03 22:19:10 +0000

    hyperkey and rectangle shortcuts dont work, just do this manually man

--- darwin/home.nix
+++ darwin/home.nix
@@ -9,11 +9,5 @@
     ".config/nvim" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/configs/nvim";
     };
-    "Library/Preferences/com.knollsoft.Rectangle.plist" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/configs/com.knollsoft.Rectangle.plist";
-    };
-    "Library/Preferences/com.knollsoft.Hyperkey.plist" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/configs/com.knollsoft.Hyperkey.plist";
-    };
   };
 }
