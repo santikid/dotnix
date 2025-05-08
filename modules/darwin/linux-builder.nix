227f1ad3f5210fa16591981b8380c0d0b368ee15
Lukas Santner <lukas@santi.gg>
2025-05-08 23:35:40 +0100

    format

--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -1,14 +1,16 @@
-{ config, pkgs, ... }:
-let
+{
+  config,
+  pkgs,
+  ...
+}: let
   system = "aarch64-darwin";
   pkgs-darwin = import (builtins.fetchTarball {
     # nixpkgs-24.05-darwin
     url = "https://github.com/nixos/nixpkgs/archive/bf32c404263862fdbeb6e5f87a4bcbc6a01af565.tar.gz";
     sha256 = "132bd16a1wp145wz4m16w2maz0md6y2hp0qn5x1102wkyr9gkk0n";
-  }) { inherit system; };
-in
-{
-    nix.linux-builder = {
+  }) {inherit system;};
+in {
+  nix.linux-builder = {
     enable = true;
     config = {
       nix.settings.sandbox = false;
@@ -18,10 +20,10 @@ in
     ephemeral = true;
     package = pkgs-darwin.darwin.linux-builder;
     maxJobs = 4;
-    supportedFeatures = [ "kvm" "benchmark" "big-parallel" "nixos-test" ];
-    systems = ["x86_64-linux" "aarch64-linux" ];
+    supportedFeatures = ["kvm" "benchmark" "big-parallel" "nixos-test"];
+    systems = ["x86_64-linux" "aarch64-linux"];
   };
-  nix.settings.trusted-users = [ "@admin" ];
+  nix.settings.trusted-users = ["@admin"];
   launchd.daemons.linux-builder = {
     serviceConfig = {
       StandardOutPath = "/var/log/darwin-builder.log";
@@ -29,4 +31,3 @@ in
     };
   };
 }
-
