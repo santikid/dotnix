1bbfb1cc45eee9f3c3ec0a2d488821bb13d03b62
Lukas Santner <lukas@santi.gg>
2025-05-18 13:42:01 +0000

    add hydra-builder user to devbox

--- hosts/devbox/default.nix
+++ hosts/devbox/default.nix
@@ -11,6 +11,19 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
 
+  users.users.hydra-builder = {
+    description = "Hydra Builder User";
+    home = "/home/hydra-builder";
+    shell = pkgs.bash;
+    extraGroups = [];
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJhplY56I2REexfLgVDu9n9LNrxBb0gbZgxrv+sPwIX root@santiserver-vm"
+    ];
+  };
+  nix.settings.trusted-users = [ "root" "hydra-builder" ];
+  services.openssh.settings.AllowUsers = ["hydra-builder"];
+
   sops.secrets.github-runner-token = {sopsFile = ../../secrets/devbox.yaml;};
   services.github-runners = {
     roller = {
