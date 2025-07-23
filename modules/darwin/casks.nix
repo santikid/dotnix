526b1ebe0f39cef328d46c854f947b4292ae69c0
Lukas Santner <lukas@santi.gg>
2025-07-23 10:27:16 +0100

    casks

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -1,12 +1,9 @@
 _: [
   "beekeeper-studio"
   "ghostty"
-  "handbrake"
   "iina"
   "transmission"
   "utm"
-  # xcode version manager
-  "xcodes"
   # store ssh keys in secure enclave
   "secretive"
   "zed"
