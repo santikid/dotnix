b8d2d8ab86956023278cda2758fee546b24281cd
Lukas Santner <lukas@santi.gg>
2025-12-22 21:39:07 +0100

    claude-code is too old on nixpkgs

--- packages/modules/devxtra.nix
+++ packages/modules/devxtra.nix
@@ -7,11 +7,5 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    opencode
-    claude-code
   ];
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "claude-code"
-    ];
 }
