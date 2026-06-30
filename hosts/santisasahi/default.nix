dc2da27e0403e8055c592c68dca050a5538cb587
Lukas Santner <lukas@santi.gg>
2026-06-30 20:18:38 +0200

    fairydust

--- hosts/santisasahi/default.nix
+++ hosts/santisasahi/default.nix
@@ -1,20 +1,27 @@
 {
-  config,
   lib,
   pkgs,
   inputs,
   user,
   ...
-}: {
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+}: let
+  system = pkgs.stdenv.hostPlatform.system;
+
+  allowedUnfreePackages = [
+    "1password"
+    "1password-cli"
+    "chromium"
+    "chromium-unwrapped"
+    "widevine-cdm"
+  ];
+in {
+  imports = [
+    ./fairydust-kernel.nix
+    ./hardware-configuration.nix
+  ];
+
   nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "1password"
-      "1password-cli"
-      "chromium"
-      "chromium-unwrapped"
-      "widevine-cdm"
-    ];
+    builtins.elem (lib.getName pkg) allowedUnfreePackages;
 
   boot.loader.systemd-boot = {
     enable = true;
@@ -22,76 +29,48 @@
   };
   boot.loader.efi.canTouchEfiVariables = false;
   boot.kernelParams = ["appledrm.show_notch=1"];
-  boot.supportedFilesystems = ["btrfs"];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
-    options = ["subvol=root" "compress=zstd" "noatime"];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
-    options = ["subvol=home" "compress=zstd" "noatime"];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/EFI";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
-
-  swapDevices = [];
-  zramSwap.enable = true;
-  services.fstrim.enable = true;
 
   networking.networkmanager = {
     enable = true;
     wifi.backend = "iwd";
   };
-  services.timesyncd = {
-    enable = true;
-    servers = [
-      "time.cloudflare.com"
-      "time.google.com"
-      "pool.ntp.org"
-    ];
-    fallbackServers = [
-      "time.nist.gov"
-      "0.nixos.pool.ntp.org"
-      "1.nixos.pool.ntp.org"
-    ];
+
+  services = {
+    fstrim.enable = true;
+    automatic-timezoned.enable = true;
+    blueman.enable = true;
+    tailscale.enable = true;
+    upower.enable = true;
+    power-profiles-daemon.enable = true;
+    timesyncd = {
+      enable = true;
+      servers = [
+        "time.cloudflare.com"
+        "time.google.com"
+        "pool.ntp.org"
+      ];
+      fallbackServers = [
+        "time.nist.gov"
+        "0.nixos.pool.ntp.org"
+        "1.nixos.pool.ntp.org"
+      ];
+    };
+    logind.settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchDocked = "suspend";
+      HandleLidSwitchExternalPower = "suspend";
+    };
   };
-  services.automatic-timezoned.enable = true;
 
-  hardware.asahi.extractPeripheralFirmware = true;
-  hardware.sensor.iio.enable = true;
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  services.tailscale.enable = true;
-  services.upower.enable = true;
-  services.power-profiles-daemon.enable = true;
-  services.logind.settings.Login = {
-    HandleLidSwitch = "suspend";
-    HandleLidSwitchDocked = "suspend";
-    HandleLidSwitchExternalPower = "suspend";
+  programs = {
+    _1password.enable = true;
+    librepods.enable = true;
+    _1password-gui = {
+      enable = true;
+      polkitPolicyOwners = [user.name];
+    };
   };
-
-  programs._1password.enable = true;
-  programs.librepods.enable = true;
   users.users.${user.name}.extraGroups = ["librepods"];
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [user.name];
-  };
 
   nix.settings = {
     trusted-users = ["root" "@wheel" user.name];
@@ -104,7 +83,7 @@
   };
 
   environment.systemPackages = with pkgs; [
-    inputs.zen-browser.packages.${pkgs.system}.default
+    inputs.zen-browser.packages.${system}.default
     (chromium.override {
       enableWideVine = true;
     })
