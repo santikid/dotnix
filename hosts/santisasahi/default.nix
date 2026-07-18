6209d15cd2765c223d28fba09cd89eb81b28a350
Lukas Santner <lukas@santi.gg>
2026-07-19 00:34:30 +0100

    santisasahi: remove nixos-apple-silicon binary cache

--- hosts/santisasahi/default.nix
+++ hosts/santisasahi/default.nix
@@ -91,16 +91,6 @@ in {
   };
   users.users.${user.name}.extraGroups = ["librepods"];
 
-  nix.settings = {
-    trusted-users = ["root" "@wheel" user.name];
-    extra-substituters = [
-      "https://nixos-apple-silicon.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
-    ];
-  };
-
   environment.systemPackages = with pkgs; [
     inputs.zen-browser.packages.${system}.default
     (chromium.override {
